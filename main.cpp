a50f743744c339e04248a338055585390906ed54
Milena Mello <milealm11@gmail.com>
2024-08-04 15:12:06 -0300

    [fix] outro teste

--- main.cpp
+++ main.cpp
@@ -56,9 +56,9 @@ int main(int argc, char *argv[]){
                     break;
                 case 2:
                     char nomeArquivo[64];
-                    unsigned int bytesLidos = 14;
+                    unsigned int bytesLidos = 10;
                     printf ("Digite o nome do arquivo que gostaria de baixar:");
-                    strcpy(nomeArquivo,"videoJoia.mp4");
+                    strcpy(nomeArquivo,"teste.txt");
                     //scanf("%s%n",nomeArquivo,&bytesLidos);
                     enviar = montar_pacote(TIPO_BAIXAR,bytesLidos-1,nomeArquivo,anterior,mensagens);
                     enviar_pacote(socketClient,bytesLidos-1,enviar,mensagens);
