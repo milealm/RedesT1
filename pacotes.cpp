284367c274c0583983386a4af027b02f6ebcf439
Milena Mello <milealm11@gmail.com>
2024-08-05 15:13:41 -0300

    [fix]

--- pacotes.cpp
+++ pacotes.cpp
@@ -187,10 +187,11 @@ int process_resposta(int socket,struct kermit *pacote,int decide,std::list<struc
                         printf ("num seq %d\n",pacoteJanela->seq);
                         if (!janelaClient.empty()){
                             printf ("janela front seq %d e pacoteRecebido %d\n",janelaClient.front()->seq, pacoteJanela->seq);
-                            if ((janelaClient.back()->seq != (pacoteJanela->seq-1)) || (janelaClient.back()->seq == 31 && pacoteJanela->seq != 0) ){
+                            if ((janelaClient.back()->seq != (pacoteJanela->seq-1)) && ((janelaClient.back()->seq == 31) && (pacoteJanela->seq != 0)) ){
                                 printf ("limpa limpa tudo\n");
                                 janelaClient.clear();
                                 pacoteJanela = NULL;
+                                printf ("aqui?\n");
                             }
                         }
                         if (pacoteJanela != NULL){
